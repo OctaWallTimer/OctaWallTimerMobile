4bc7af3d65178fb0b089a3459d3dbc4ce3e1c008
kurzajf <100566833+kurzajf@users.noreply.github.com>
2022-03-13 22:22:29 +0100

    Połączenie z kostką poprzez bluetooth

--- App.tsx
+++ App.tsx
@@ -1,10 +1,57 @@
 import { StatusBar } from 'expo-status-bar';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import base64 from 'react-native-base64'
+
+import { manager, SERVICE_UUID, CHARACTERISTIC_UUID } from './BLE';
+import { Characteristic } from 'react-native-ble-plx';
 
 export default function App() {
+  let [characteristic, setCharacteristic] = useState<Characteristic|null>(null);
+  let [wall, setWall] = useState<number>(-1);
+  React.useEffect(() => {
+    const subscription = manager.onStateChange((state) => {
+        if (state === 'PoweredOn') {
+          manager.startDeviceScan(null, null, (error, device) => {
+            if (error || !device) {
+              console.log("Cant connect to device, " + error?.reason);
+              return
+            }
+            if (device.name === 'OctaWallTimer') {   
+              manager.stopDeviceScan();
+              device.connect()
+              .then((device) => {
+                  return device.discoverAllServicesAndCharacteristics()
+              })
+              .then((device) => {
+                return device.readCharacteristicForService(SERVICE_UUID, CHARACTERISTIC_UUID);
+              })
+              .then((characteristic) => {
+                setCharacteristic(characteristic);
+              })
+              .catch((error) => {
+                console.log(error?.reason);
+              });
+            }
+          });
+          subscription.remove();
+        }
+    }, true);
+    return () => subscription.remove();
+  }, [manager]);
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      characteristic?.read().then(c => c.value == null ? -1 : setWall(base64.decode(c.value).charCodeAt(0)));
+    }, 500);
+    return () => clearInterval(id);
+  }, [characteristic]);
+
+
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Sciana: {wall}</Text>
       <StatusBar style="auto" />
     </View>
   );
---
+++ BLE.tsx
@@ -0,0 +1,5 @@
+import { BleManager } from 'react-native-ble-plx';
+
+export const manager = new BleManager();
+export const SERVICE_UUID = "d9feba64-a2e0-11ec-b909-0242ac120002";
+export const CHARACTERISTIC_UUID = "e0c21b3e-a2e0-11ec-b909-0242ac120002";
